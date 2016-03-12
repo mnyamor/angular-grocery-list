1340fb8f5bd78dfbdfbc61a2ec036609af3397da
Mary Nyamor <mnyamor@gmail.com>
2016-03-12 11:22:15 +0200

    load libraries

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
 /*
-* created by Mary Nyamor
+* created by Mary Nyamor on 11.03.2016
 */
 
-var app = angular.module('groceryApp', [])
+var app = angular.module('groceryListApp', ['ngRoute'])
+    .controller('HomeCtrl', ['$scope', function($scope){
+        $scope.appTitle ='Grocery List';
+    }])
+
+    .controller('GroceryListItemCtrl', ['$scope', function($scope){
+        $scope.groceryItems = [
+            { completed:true, itemName:'cookies', date:'2016-03-11'},
+            { completed:true, itemName:'milk', date:'2016-03-04'},
+            { completed:true, itemName:'bananas', date:'2016-03-08'},
+            { completed:true, itemName:'apples', date:'2016-03-10'},
+            { completed:true, itemName:'chocolate', date:'2016-03-02'}
+        ];
+    }])
+
+    .config(function($routeProvider){
+           /* $routeProvider
+                when.('/', {
+                    template:'views/list.html',
+                    controller:'homectrl'
+                })
+                when.('/add', {
+                    template:'views/add.html',
+                    controller:'addGroceryCrl'
+                })
+        otherwise({
+            redirectTo:'/'
+        });*/
+    });
---
+++ js/controllers/groceryController.js
@@ -0,0 +1,10 @@
+app.module('groceryCtrlModule', [])
+
+    .controller('GroceryCtrl',['$scope', function($scope){
+
+
+    }])
+    .controller('addGroceryCrl',['$scope', function($scope){
+
+
+    }])
